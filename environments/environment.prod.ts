0fbafb62183a9ac6141b4eecb892525b23f8ee73
yasirbutt01 <111048405+yasirbutt01@users.noreply.github.com>
2022-08-11 15:00:49 +0500

    Add files via upload

---
+++ environments/environment.prod.ts
@@ -0,0 +1,19 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyC4RNNlqXSwTM1Kt6Pbgutq2yuavSQ9WnU',
+    authDomain: 'ismaestro-angularexampleapp.firebaseapp.com',
+    databaseURL: 'https://ismaestro-angularexampleapp.firebaseio.com',
+    projectId: 'ismaestro-angularexampleapp',
+    storageBucket: 'ismaestro-angularexampleapp.appspot.com',
+    messagingSenderId: '965114235515'
+  },
+  api: {
+    //baseUrl: 'https://easypeasydevapi.stagingdesk.com'
+    //baseUrl: 'https://clientstagingapi.easypeasycards.app'
+    // baseUrl: 'https://liveapi.easypeasycards.app'
+    // baseUrl: 'https://earwaxapidev.stagingdesk.com'
+    // baseUrl:'https://localhost:44303'
+  baseUrl: 'https://api.surreyearwaxclinic.com'
+  }
+};
